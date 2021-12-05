7d68ce4e7fe49c064f72c054a7da4f4a28942ed7
Alejandro <aleolmos03@gmail.com>
2021-12-05 19:15:40 -0300

    modificacion Seeder de cero

--- database/seeders/EventTypesTableSeeder.php
+++ database/seeders/EventTypesTableSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DonationsTypesTableSeeder extends Seeder
+class EventTypesTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
